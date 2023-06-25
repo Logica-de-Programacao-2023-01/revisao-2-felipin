c01d73d0e7cc9fe7f4ef90247f90da511bf0b80a
Bruno César <79723563+brunodeev@users.noreply.github.com>
2023-06-25 13:04:32 -0300

    q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -2,15 +2,6 @@ package q2
 
 import "errors"
 
-//Você é um desenvolvedor de software em uma empresa financeira e está trabalhando em um sistema de folha de pagamento.
-//Cada funcionário possui um ID único, nome, cargo, salário base e um conjunto de bônus mensais. Você decidiu usar uma
-//struct para representar as informações de cada funcionário.
-//
-//Agora, você precisa implementar uma função chamada "calculateTotalSalary" que recebe como parâmetro um ponteiro para um
-//objeto do tipo Employee e calcula o salário total do funcionário, considerando o salário base e a soma dos bônus
-//mensais. Caso a soma dos bônus mensais seja maior que 1500.0, a titulação do funcionário deve ser atualizada com o
-//prefixo "Senior".
-
 type Employee struct {
 	ID         int
 	Name       string
