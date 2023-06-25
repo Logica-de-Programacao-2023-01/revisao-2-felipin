1cd3f4439a7326b9ba18f6694ed40806d76e9d01
Bruno César <79723563+brunodeev@users.noreply.github.com>
2023-06-25 13:06:15 -0300

    q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -11,16 +11,18 @@ type Employee struct {
 }
 
 func CalculateTotalSalary(employee *Employee) (float64, error) {
-	totalBonus := 0.0
-	for _, bonus := range emp.MonthlyBonus {
-		totalBonus += bonus
+	if employee == nil {
+		return 0.0, errors.New("Funcionario invalido")
+	} else {
+		totalbonus := 0.0
+		for _, bonus := range employee.Bonuses {
+			totalbonus += bonus
+		}
+		totalsalary := employee.BaseSalary + totalbonus
+
+		if totalbonus > 1500.0 {
+			employee.Title = "Senior " + employee.Title
+		}
+		return totalsalary, nil
 	}
-
-	totalSalary := emp.BaseSalary + totalBonus
-
-	if totalBonus > 1500.0 {
-		emp.Position = "Senior " + emp.Position
-	}
-
-	return totalSalary, nil
 }
